6e27229458a7f0ce2a18a2817f338685a4903374
Narima <47755927+Karambamba@users.noreply.github.com>
2024-08-16 18:59:55 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,41 @@
+/*Slider*/
+const images = document.querySelectorAll("#slider img");
+const title = document.querySelector("#title");
+let currentIndex = 0;
+const interval = 10000;
+
+function changeImage() {
+  images[currentIndex].style.opacity = 0;
+  currentIndex = (currentIndex + 1) % images.length;
+  images[currentIndex].style.opacity = 1;
+}
+
+images.forEach((img, index) => {
+  img.style.opacity = index === currentIndex ? 1 : 0;
+});
+
+setInterval(changeImage, interval);
+
+const h1 = document.getElementById("titleCounter");
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach((entry) => {
+    if (entry.isIntersecting) {
+      // Додаємо клас visible, щоб зробити текст видимим
+      h1.classList.add("visible");
+      // Прибираємо спостерігача після першого спрацьовування
+      observer.unobserve(h1);
+    }
+  });
+});
+
+// Спостереження за елементом h1
+observer.observe(h1);
+
+function start() {
+  window.setInterval(timer, 50);
+}
+
+function timer() {
+  var elem = document.getElementById("counter");
+  elem.value = parseInt(elem.value) - 1;
+}
